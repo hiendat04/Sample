2c9c7b3d4d618c00082a14472ad4ae57a751bfc9
Dat Hien Mai <hiendat04@gmail.com>
2024-12-31 11:04:55 +0700

    CORS configuration: option 2

--- src/main/java/com/demo/sample/configuraion/AppConfig.java
+++ src/main/java/com/demo/sample/configuraion/AppConfig.java
@@ -1,19 +1,22 @@
 package com.demo.sample.configuraion;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+public class AppConfig {
+    @Bean
+    public WebMvcConfigurer corsFilter() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // TODO Auto-generated method stub
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173/");
+            }
+        };
     }
 
 }
